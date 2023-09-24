159635df2321eec5b4f5165430b3210fb390ed16
Annette McArdle <Annette.McArdle@customerservice.nsw.gov.au>
2023-09-24 11:55:06 +1000

    Updating ReadMe, Linting, and Comments.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# -Assessment-Task
-NodeJS Lambda Function for Address Lookup
+# Assessment-Task
+
+/********************************************************************************************************
+
+- Title: Assessment Task - NodeJS Lambda Function for Address Lookup
+- Author: Annette McArdle
+- Date: 22/09/2023
+- Input: JSON Object, eg: {"address":"39 mayfield road oberon"}
+- Output: JSON Object, eg:
+- {
+- data: {
+        address: '39 MAYFIELD ROAD OBERON',
+        location: [ 149.7877314258029, -33.62593407349641 ],
+        suburbName: 'OBERON',
+        stateElectoralDistrictName: 'BATHURST'
+       },
+       error: []
+  }
+- Description: Lambda function, that takes an address, fetches the coordinates from NSW_Geocoded_Addressing_Theme
+- then, using those coordinates makes 2 parallel requests,
+- one to NSW_Administrative_Boundaries_Theme/FeatureServer/4 to fetch the district name
+- the other, to NSW_Administrative_Boundaries_Theme/FeatureServer/2 to fetch the suburb
+- if no address is found, the promise resolves with a 200 status, but the response contains a message relaying that no address was found.
+- if the inital request to fetch coordinates fails, the promise rejects, and error message is returned. No subsequent requests are possible.
+- if the coordinates are fetched, but one of the subsequent requests fails, the promise resolves, a 200 status is returned with data that was able
+- to be obtained and an error message for the data that could not be fetched is also returned
+  *****************************************************************************************************/
+
+Prerequisites: NodeJS 18.x or higher
+Install: npm install
+Run local integration test: node test/local.js
+Run eslint: npm run pretest
+Run Unit tests: npm run test
--- index.js
+++ index.js
@@ -1,8 +1,44 @@
+/*********************************************************************************************************
+ * Title: Assessment Task
+ * Author: Annette McArdle
+ * Date: 22/09/2023
+ * Input: JSON Object, eg: {"address":"39 mayfield road oberon"}
+ * Output: JSON Object, eg:
+ *      {
+ *          data: {
+				address: '39 MAYFIELD ROAD OBERON',
+				location: [ 149.7877314258029, -33.62593407349641 ],
+				suburbName: 'OBERON',
+				stateElectoralDistrictName: 'BATHURST'
+			},
+			error: []
+		}
+ * Description: Lambda function, that takes an address, fetches the coordinates from NSW_Geocoded_Addressing_Theme
+ *              then, using those coordinates makes 2 parallel requests,
+ *                    one to NSW_Administrative_Boundaries_Theme/FeatureServer/4 to fetch the district name
+ *                    the other, to NSW_Administrative_Boundaries_Theme/FeatureServer/2 to fetch the suburb
+ *              if no address is found, the promise resolves with a 200 status, but the response contains a message relaying that no address was found.
+ *              if the inital request to fetch coordinates fails, the promise rejects, and error message is returned. No subsequent requests are possible.
+ *              if the coordinates are fetched, but one of the subsequent requests fails, the promise resolves, a 200 status is returned with data that was able
+ *                 to be obtained and an error message for the data that could not be fetched is also returned
+ ********************************************************************************************************/
 const axios = require('axios');
 
 exports.handler = function (event, context, callback) {
 	console.log("Assignment Event: ", event);
 
+	//creating a return object allows me to control the key order, when suburb and district can return in, and then be added either order
+	let toReturn = {
+		data: {
+			address: '',
+			location: [],
+			suburbName: '',
+			stateElectoralDistrictName: ''
+		},
+		error: []
+	};
+
+	//axios request config to NSW_Geocoded_Addressing_Theme
 	let config = {
 		method: 'get',
 		maxBodyLength: Infinity,
@@ -16,31 +52,20 @@ exports.handler = function (event, context, callback) {
 		}
 	};
 
-	// console.log("URL", config.url);
-	// console.log("Params: ", config.params);
-
-	let toReturn = {
-		data: {
-			address: '',
-			location: [],
-			suburbName: '',
-			stateElectoralDistrictName: ''
-		},
-		error: []
-	}
+	console.log("Request: ", config.url, "where=",config.params.where);
 	axios.request(config)
 		.then((response) => {
 
-			// console.log("Query1 Response: ", JSON.stringify(response.data.features));
-			// console.log("Query1 Response Length: ", response.data.features.length);
-
 			if (response.data.features.length > 0) {
-				// console.log("Coordinates: ", response.data.features[0].geometry.coordinates.join());
 				toReturn.data.address = response.data.features[0].properties.address;
 				toReturn.data.location.push(response.data.features[0].geometry.coordinates[0]);
 				toReturn.data.location.push(response.data.features[0].geometry.coordinates[1]);
 
+				//need a promise array and Promise.AllSettled to ensure the two concurrent requests both finish and the lambda function doesn't return prematurely.
 				let promiseArray = [];
+
+				// I use separate configs and response for each request.
+				// Config/response relates to fetching coordinates. Config2/response2 relates to the districtname request. Config3/response3 relates to suburb
 
 				let config2 = {
 					method: 'get',
@@ -57,9 +82,9 @@ exports.handler = function (event, context, callback) {
 					}
 				};
 
+				console.log("Request: ", config2.url, "geometry=",config2.params.geometry, "outFields: ", config2.params.outFields);
 				let promise2 = axios.request(config2)
 					.then((response2) => {
-						console.log("Query2: ", JSON.stringify(response2.data));
 						if (response2.data.features.length > 0) {
 							toReturn.data.stateElectoralDistrictName = response2.data.features[0].properties.districtname;
 						}
@@ -91,9 +116,10 @@ exports.handler = function (event, context, callback) {
 					}
 				};
 
+				console.log("Request: ", config3.url, "geometry=",config3.params.geometry, "outFields: ", config3.params.outFields);
 				let promise3 = axios.request(config3)
 					.then((response3) => {
-						console.log("Query3: ", JSON.stringify(response3.data));
+
 						if (response3.data.features.length > 0) {
 							toReturn.data.suburbName = response3.data.features[0].properties.suburbname;
 						}
@@ -112,14 +138,14 @@ exports.handler = function (event, context, callback) {
 					});
 				promiseArray.push(promise3);
 
-				Promise.allSettled(promiseArray).then((results) => {
-					// console.log("Results: ", JSON.stringify(results));
-					callback(null, toReturn);
-				})
-				.catch((error) => {
-					console.log(error);
-					callback(null, toReturn);
-				});
+				Promise.allSettled(promiseArray)
+					.then(() => {
+						callback(null, toReturn);
+					})
+					.catch((error) => {
+						console.log(error);
+						callback(null, toReturn);
+					});
 			} else {
 				toReturn.error.push({
 					code: 404,
